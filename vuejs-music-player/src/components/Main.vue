93bb62db3bf31fe3737ccae7ba1b18d1a8551036
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-02-19 11:31:52 -0800

    Displayed PlayerControls Component

--- vuejs-music-player/src/components/Main.vue
+++ vuejs-music-player/src/components/Main.vue
@@ -2,11 +2,17 @@
     <div>
         <h1>Main Component</h1>
     </div>
+    <PlayerControls />
 </template>
 
 <script>
+import PlayerControls from './PlayerControls/PlayerControls.vue'
+
 export default {
   name: 'Main',
+  components: {
+    PlayerControls
+  },
   props: {
   }
 }
---
+++ vuejs-music-player/src/components/PlayerControls/PlayerControls.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>PlayerControls Component</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PlayerControls",
+    components: {
+        
+    }
+}
+</script>
