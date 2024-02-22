174361a19d32268e3af9ae0d5c2938c020530a8f
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-02-21 20:22:27 -0800

    Moved AlbumCover import to SongList

--- vuejs-music-player/src/components/Main.vue
+++ vuejs-music-player/src/components/Main.vue
@@ -1,14 +1,13 @@
 <template>
     <div>
         <h1>VueJS Music Player</h1>
-        <AlbumCover />
         <PlayerControls />
     </div>
 </template>
 
 <script>
 import PlayerControls from './PlayerControls/PlayerControls.vue'
-import AlbumCover from './ArtistInfo/AlbumCover.vue'
+// import AlbumCover from './ArtistInfo/AlbumCover.vue'
 import sampleData from '../assets/sampleData.js'
 
 export default {
