5614ee0b1ec8e818c5ffe1a9b7d14201e6801229
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-02-28 23:35:15 -0800

    Added song to sampleSongs

--- vuejs-music-player/src/assets/sampleData.js
+++ vuejs-music-player/src/assets/sampleData.js
@@ -1,18 +1,19 @@
 import Starlight from "./sampleSongs/Martin Garrix, Dubvision ft. Shaun Farrugia - Starlight (Keep Me Afloat) (Extended Mix).mp3"
 import Atmosphere from "./sampleSongs/FISHER x Kita Alexander - Atmosphere (Extended Mix).mp3"
+import MoveYourBody from "./sampleSongs/Öwnboss & Sevek - Move Your Body (Extended Mix).mp3"
 
 const testData = [
-    {
-        song: "Funky Town",
-        artist: "ACRAZE",
-        songFile: "sampleSong.mp3",
-        albumCoverFile: "sampleAlbumCover.png"
-    },
     {
         song: "Atmosphere",
         artist: "FISHER",
         songFile: Atmosphere,
         albumCoverFile: "sampleAlbumCover2.png"
+    },
+    {
+        song: "Move Your Body",
+        artist: "Ownboss & Sevek",
+        songFile: MoveYourBody,
+        albumCoverFile: "sampleAlbumCover.png"
     },
     {
         song: "Starlight",
