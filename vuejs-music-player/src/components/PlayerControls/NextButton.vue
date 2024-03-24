6ecb7549c4a2ccc840aab2f3616b01c6323a9013
Jean-Luc Felix <jlfelix522@yahoo.com>
2024-03-23 18:37:21 -0700

    Rendered Next Button with CSS

--- vuejs-music-player/src/components/PlayerControls/NextButton.vue
+++ vuejs-music-player/src/components/PlayerControls/NextButton.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
-        <button v-on:click="pressNext()">Next</button>
+        <img 
+            src="../../assets/playerControlButtons/NextButton.png"
+            className="nextButtonImage"
+        />
+        <!-- <button v-on:click="pressNext()">Next</button> -->
     </div>
 </template>
 
 <script>
+
+import "./NextButton.css"
 
 export default {
     name: "NextButton",
